877fda22a4ba4a973dd891967dafc68d9c016007
devitasari <devitas700@gmail.com>
2019-12-12 20:46:35 +0700

    add home page create room

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    playerCount: 0,
+    player: '',
+    roomId: ''
   },
   mutations: {
+    addPlayer (state, { payload }) {
+      this.state.playerCount++
+      this.state.player = {
+        [`name${this.state.playerCount}`]: payload
+      }
+      console.log(this.state)
+    }
   },
   actions: {
+    createRoom ({ commit }, payload) {
+      commit('addPlayer', { payload })
+      // db.colllection('room').add({
+      //   playerCount: this.playerCount,
+      //   player: this.player
+      // })
+      //   .then((docRef) => {
+      //     this.roomId = docRef.id
+      //   })
+      //   .catch(err => {
+      //     console.error('Error adding document: ', err)
+      //   })
+    }
   },
   modules: {
   }
