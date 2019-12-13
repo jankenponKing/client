2f154325132681f91642aa368f715fec07b6e1b5
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2019-12-13 10:28:20 +0700

    jhw create room, join roomin progress

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from '../../config/firebase'
+import router from '@/router'
+import Swal from 'sweetalert2'
+
 
 Vue.use(Vuex)
 
@@ -7,8 +11,12 @@ export default new Vuex.Store({
   state: {
     playerCount: 0,
     player: '',
-    roomId: ''
+    linkRoom: '',
+    objectData: {},
+    username: '',
+    master: false
   },
+
   mutations: {
     addPlayer (state, { payload }) {
       this.state.playerCount++
@@ -16,23 +24,105 @@ export default new Vuex.Store({
         [`name${this.state.playerCount}`]: payload
       }
       console.log(this.state)
-    }
+    },
+    addData(state, payload)
+      {
+        console.log('TCL \n============\n jalan nih addData mutations' )
+        this.state.objectData = payload
+        console.log('TCL \n============\n ', this.state.objectData)
+      },
+    changeName(state, payload)
+      {
+        state.username = payload
+      },
+    changeRoom(state, payload)
+      {
+        state.linkRoom = payload
+      },
+    changePlayerCount(state, payload)
+      {
+        this.playerCount = payload
+      },
+    changePlayStatus(state, payload)
+      {
+        state.playStatus = payload
+      },
+    changeMaster(state)
+      {
+        this.master = true
+      }
   },
+
   actions: {
-    createRoom ({ commit }, payload) {
-      commit('addPlayer', { payload })
-      // db.colllection('room').add({
-      //   playerCount: this.playerCount,
-      //   player: this.player
-      // })
-      //   .then((docRef) => {
-      //     this.roomId = docRef.id
-      //   })
-      //   .catch(err => {
-      //     console.error('Error adding document: ', err)
-      //   })
-    }
-  },
+    createRoom({ commit, state }, playername)
+      {
+        Swal.fire({
+          imageUrl: 'https://clipart4biz.com/images/transparent-background-loading-3.gif',
+          text: 'Creating your room...',
+          imageWidth: 200,
+          imageHeight: 200,
+          showConfirmButton: false
+        })
+
+        db.collection('room').add({
+          maxPlayer:2,
+          playStatus:false,
+          player1: {
+            username : playername,
+            option: '',
+          },
+          winner:''
+        })
+        .then(docRef=>{
+          console.log('TCL \n============\n ', docRef) 
+
+          commit('changeRoom', docRef.id)
+          commit('changePlayerCount', 1)
+          // commit('changeName', payload)
+          commit('changeMaster')
+
+          router.push(`/joinRoom/${state.linkRoom}`)
+
+          Swal.close()
+        })
+        .catch(err=>{
+          console.log('TCL \n============\n ', err)
+        })
+      },
+    fetchData({ commit, state }, payload)
+      {
+        console.log('TCL \n============\n ', 'masuk sini fetch data')
+        commit('changeRoom', payload)
+        db.collection('room').doc(`${payload}`)
+          .onSnapshot((doc)=>
+            {
+              console.log('TCL \n============\n ', 'masuk sinio on snapshot')
+              console.log("TCL: doc", doc)
+              commit('addData', doc.data())
+              console.log('TCL \n============\n doc.data ', doc.data())
+              console.log('TCL \n============\n objec data ', this.state.objectData)
+            }
+        )
+      },
+    addPlayer ({ commit, state }, payload) {
+      // let countPlayer = state.objectData.count + 1
+      commit('changePlayerCount', 2)
+      db.collection('room').doc(`${payload.room}`).update({
+          player2: {
+              username : payload.newuser,
+              option: '',
+            },
+        })
+        .then(function () {
+          commit('changeName', payload.newuser)
+          console.log()
+          console.log('Document successfully written!')
+        })  
+        .catch(function (error) {
+          console.error('Error writing document: ', error)
+        })
+    },
+      },
   modules: {
-  }
+    }  
 })
--- src/views/joinRoom.vue
+++ src/views/joinRoom.vue
@@ -1,60 +1,158 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+    <b-container class="pb-3">
+    <b-row>
+      <b-col>
+        <h1 class="welcome-title">Join with "{{ getTotalPlayer }}" in this room!</h1>
+      </b-col>
+    </b-row>
+
+    <b-row class="justify-content-center my-4">
+      <b-col cols="7">
+
+      <form @submit.prevent="userJoinRoom">
+        <b-input-group>
+          <b-form-input v-model="newuser" placeholder="Enter your name" type="text"></b-form-input>
+          <b-input-group-append>
+            <b-button type="submit" variant="outline-secondary">Button</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </form>
+
+      </b-col>
+    </b-row>
+
+
+    <b-row>
+      <b-col>
+        <b-button v-if="getMaster" @click="playNow" class="btn-lg my-3" variant="success">PLAY!</b-button>
+      </b-col>
+    </b-row>
+
+    <b-row class="justify-content-center mt-4">
+      <b-col cols="6">
+        <div v-for="(data,key) in getData" :key="key">
+          <b-card v-if="data.username" class="text-center background-color-XXX mb-2">
+            <div class="text-white" style="font-size: 30px;">
+              {{ data.username }}
+            </div>
+          </b-card>
+        </div>
+      </b-col>
+    </b-row>
+
+  </b-container>
+
+
+
+
+
+
+
+
+
+
   </div>
 </template>
 
+
+
+
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'joinRoom',
+  data: function(){
+    return{
+      formUserName: true,
+      newUser: '',
+
+    }
+  },
+  methods:{
+    fetchData()
+      {
+        console.log('TCL \n============\n ', 'jalan nih fetch data')
+        this.$store.dispatch('fetchData', this.$route.params.room)
+      },
+    userJoinRoom () 
+      {
+        console.log('TCL \n============\n ', this.newuser)
+        this.$store.dispatch('addPlayer', { room: this.$route.params.room, newuser: this.newuser })
+      },
+    playNow () 
+      {
+        this.$store.dispatch('playNow', this.$route.params.room)
+      }
+  },
+  created(){
+    console.log('TCL \n============\n ', 'jalan nih created')
+    this.fetchData()
+  },
+  computed: {
+    getData () {
+      const data = Object.values(this.$store.state.objectData)
+      const arr = data.slice(2, data.length - 1)
+      for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+          if (arr[j].playerOrder > arr[i].playerOrder) {
+            var tampung = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tampung
+          }
+        }
+      }
+      return arr
+    },
+    getTotalPlayer () {
+      return this.$store.state.objectData.player1.username
+    },
+    getUser () {
+      return this.$store.state.username
+    },
+    startGame () {
+      return this.$store.state.objectData.playStatus
+    },
+    getMaster () {
+      return this.$store.state.master
+    }
+  },
+  watch: {
+    startGame (value) {
+      if (value) {
+        this.$router.push({ path: `/game/${this.$route.params.room}` })
+      }
+    }
+  },
+  methods: {
+    fetchData () {
+      this.$store.dispatch('fetchData', this.$route.params.room)
+    },
+    userJoinRoom () {
+      this.$store.dispatch('addPlayer', { room: this.$route.params.room, newuser: this.newuser })
+    },
+    playNow () {
+      this.$store.dispatch('playNow', this.$route.params.room)
+    }
+  },
+  created () {
+    this.fetchData()
   }
-}
+
+  }
+  
+
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+ @import url('https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+
+  .welcome-title{
+    font-family: 'Quicksand', sans-serif;
+  }
+  .background-color-XXX{
+    background-color: #62B69B;
+  }
 </style>
