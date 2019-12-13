f0c5d57e6ef4e1846aab2591a9861313e540abb0
Carl <carlh3h@yahoo.co.id>
2019-12-13 10:57:25 +0700

    simple game page without vuex store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <h1>Gunting Batu Kertas</h1>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -10,7 +11,7 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-
-    <label>Player 1</label>
-    <input v-model="player1">
+    <!-- <h1>This is an about page</h1> -->
+    <div>
+      <label>{{player1Name}}</label>
+      <div :class='{chosen: player1 == "gunting"}' @click='playerOneChoose("gunting");'>Gunting</div>
+      <div :class='{chosen: player1 == "batu"}' @click='playerOneChoose("batu")'>Batu</div>
+      <div :class='{chosen: player1 == "kertas"}' @click='playerOneChoose("kertas")'>Kertas</div>
+    </div>
     <br>
-    <label>Player 2</label>
-    <input v-model="player2">
+    <div v-if='finished'>
+      <label>{{player2Name}}</label>
+      <div :class='{chosen: player2 == "gunting"}' @click='playerTwoChoose("gunting")'>Gunting</div>
+      <div :class='{chosen: player2 == "batu"}' @click='playerTwoChoose("batu")'>Batu</div>
+      <div :class='{chosen: player2 == "kertas"}' @click='playerTwoChoose("kertas")'>Kertas</div>
+    </div>
     <br>
     <button @click="getWinner">Play</button>
     <label>Winner</label>
@@ -20,7 +27,10 @@ export default {
     return {
       player1: '',
       player2: '',
-      winner: ''
+      player1Name: 'player #1',
+      player2Name: 'player #2',
+      winner: '',
+      finished: ''
     }
   },
   methods: {
@@ -30,11 +40,26 @@ export default {
       else if (this.player1 === 'kertas' && this.player2 === 'batu') this.winner = 'Player 1'
       else if (this.player1 === this.player2) this.winner = 'Match'
       else this.winner = 'Player 2'
+    },
+    playerOneChoose(option){
+      this.player1 = option
+      if(this.player2 != ''){
+        this.getWinner()
+      }
+    },
+    playerTwoChoose(option){
+      this.player2 = option
+      if(this.player1 != ''){
+        this.getWinner()
+      }
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .chosen{
+    background-color: lightgreen;
+    border: 1px solid black;
+  }
 </style>
